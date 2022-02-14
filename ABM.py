04e2e06d2b9ff2cdc510d200eac2bd6f1c883849
robmacg1 <98338791+robmacg1@users.noreply.github.com>
2022-02-14 17:58:48 +0000

    Add files via upload

--- ABM.py
+++ ABM.py
@@ -8,32 +8,16 @@ import agentframework as af
 
 rn.seed(19)
 
-def distance_between(a, b):
-    """
-    This is a function that calculates the distance betweeen two pairs of coordinates.
 
-    Parameters
-    ----------
-    a : List with at least 2 items, y coordinate and x coordinate
-        This list refers to the location of the first agent
-    b : List with at least 2 items, y coordinate and x coordinate
-        This list refers to the location of the second agent
-
-    Returns
-    -------
-    Float
-        Euclidean distance between the 2 pairs of coordinates
-
-    """
-    return (((a.x - b.x)**2) + ((a.y - b.y)**2))**0.5
 
 
 ## Number of agents variable:
-num_of_agents = 15
+num_of_agents = 30
 ## Number of iterations variable
 num_of_iterations = 100
+neighbourhood = 20
 ## Possible speed variable
-s = 3
+s = 4
 ## create empty envirnment list
 environment = []
 store = 5
@@ -47,40 +31,59 @@ with open('env.csv', newline="") as e:
         for value in row:
             rowlist.append(value)
         environment.append(rowlist)
-    #matplotlib.pyplot.imshow(environment)
-    #matplotlib.pyplot.show()
+    matplotlib.pyplot.imshow(environment)
+    matplotlib.pyplot.show()
 e.close()
-## copy environment list to keep a list of elevations
-elevation = environment.copy()
 
-   
+
+## copy environment list to keep a list of elevations
+#elevation = environment.copy()
+#
+#environment[0][0] = -1
+#print(elevation[0][0])
+# this was a test to see i fthe 2 rasters were independent
+print(len(environment[0]))
+ncol = len(environment[0])
+elevation = []
+for i in range(len(environment)):
+    rowlist = []
+    elevation.append(rowlist)
+    for j in range(ncol):
+        print(environment[i][j])
+        elevation[i][j] = environment[i][j]
+
+environment[0][0] = -1
+print(elevation[0][0])
+# this was a test to see i fthe 2 rasters were independent        
+    
 ## create empty agents list
 agents = []
 
 ## add i of agent class to agents list
 for i in range(num_of_agents):
-    agents.append(af.Agent(i, environment, store, elevation))
+    agents.append(af.Agent(i, environment, agents, store, elevation))
 
 ## Randomly move each agent, make them eat and plot the new environment 
 for i in range(num_of_iterations):
    for j in range(num_of_agents):
        agents[j].move(s)
        agents[j].eat()
+       agents[j].share_w_neighbours(neighbourhood)
        print(agents[j])
    matplotlib.pyplot.xlim(0, 249)
    matplotlib.pyplot.ylim(0, 249)
    matplotlib.pyplot.imshow(environment, vmin = 0, vmax = 255)
-for i in range(num_of_agents):
-    matplotlib.pyplot.scatter(agents[i].x,agents[i].y, c = 'white', s=agents[i].store * 0.2)
-matplotlib.pyplot.show()
+   for i in range(num_of_agents):
+       matplotlib.pyplot.scatter(agents[i].x,agents[i].y, c = 'white', s=agents[i].store * 0.2)
+   matplotlib.pyplot.show()
 
    
        
 #for i in range(num_of_agents):
 #   print(agents[i])
 
-## Create empty list for distances
-distances = []
+
+
 """
 for j in range(0,num_of_agents,1):
     for k in range(j+1,num_of_agents,1):
--- agentframework.py
+++ agentframework.py
@@ -4,7 +4,7 @@ import random as rn
 #store = 5
 
 class Agent:
-    def __init__(self, i, environment, store, elevation):
+    def __init__(self, i, environment, agents, store, elevation):
         self.id = i
         self.x = rn.randint(0,249)
         self.y = rn.randint(0,249)
@@ -12,12 +12,57 @@ class Agent:
         self.store = store
         self.count = 0
         self.elevation = elevation
+        self.agents = agents
         
     def __str__(self):
-        return "id=" + str(self.id) + ", x=" + str(self.x) + ", y=" + str(self.y) + ", store =" + str(self.store) + ", count =" + str(self.count)
+        return ("id=" + str(self.id) + ", x=" + str(self.x) + ", y=" + str(self.y) + 
+                ", store =" + str(self.store) + ", count =" + str(self.count))
+    
+    def distance_between(a, b):
+            """
+            This is a function that calculates the distance betweeen two pairs of coordinates.
+        
+            Parameters
+            ----------
+            a : List with at least 2 items, y coordinate and x coordinate
+                This list refers to the location of the first agent
+            b : List with at least 2 items, y coordinate and x coordinate
+                This list refers to the location of the second agent
+        
+            Returns
+            -------
+            Float
+                Euclidean distance between the 2 pairs of coordinates
+        
+            """
+            return (((a.x - b.x)**2) + ((a.y - b.y)**2))**0.5    
+        
+    def share_w_neighbours(self, neighbourhood):
+        """
+        
 
-   
-             
+        Parameters
+        ----------
+        neighbourhood : Number
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+        for i in self.agents:
+            distance = self.distance_between(i)
+            if distance <= neighbourhood:
+                sum = self.store + i.store
+                avg = sum/2
+                self.store = avg
+                i.store = avg
+    
+           
+    
+
+
     def move_xory(self, xory, s):
         """
         Move coordinates
@@ -62,16 +107,16 @@ class Agent:
         y = self.move_xory(self.y, s)
         dif = abs(self.elevation[self.y][self.x] - self.elevation[y][x])
         print(str(self.elevation[self.y][self.x]) + ", " + str(self.elevation[y][x]) + ", "  + str(dif))
-        if dif < 20:
+        if dif < 50:
             self.x = x
             self.y = y
-        if self.store > s:
-            self.store = self.store - (s-1)
+            if self.store > s:
+                self.store = self.store - (s-1)
         
     def eat(self): # can you make it eat what is left?
         if self.environment[self.y][self.x] > 20 and rn.random() < 0.8:
             self.environment[self.y][self.x] -= 20
-            """
+            self.store += 10 # half calories lost to life processes 
             self.environment[self.y-1][self.x-1] -=3
             self.environment[self.y][self.x-1] -=3
             self.environment[self.y-1][self.x] -=3
@@ -80,8 +125,14 @@ class Agent:
             self.environment[self.y][self.x+1] -=3
             self.environment[self.y-1][self.x+1] -=3
             self.environment[self.y+1][self.x-1] -=3
-            """
-            self.store += 10 # half calories lost to life processes
+           
+        else:
+            a = self.environment[self.y][self.x]
+            self.store + (a/2)
+            a - a
+            
+           
+            
         #else:
         #    self.environment[self.y][self.x] +=10
             
